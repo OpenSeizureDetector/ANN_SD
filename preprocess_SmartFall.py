1eaadbaf48d360ef5a33a79f20e827d5e0edbaa5
Graham Jones <grahamjones139@gmail.com>
2019-04-20 21:55:11 +0100

    Sorted issue with preprocess_smartfall giving negative magnitude values (because we were re-sampling the magniude values rather than re-sampling the XYZ values, then calculating magnitude at the end).

--- preprocess_SmartFall.py
+++ preprocess_SmartFall.py
@@ -9,7 +9,6 @@ import numpy as np
 import numpy.linalg as la
 import scipy.signal
 import matplotlib.pyplot as plt
-#from imutils import paths
 
 RAW_FREQ = 31.25  # Hz
 PROC_FREQ = 25.  # Hz
@@ -19,6 +18,8 @@ def getMagnitude(inArr):
     """ Return an array that is the magnitude of the three column inArr
     """
     outArr = la.norm(inArr,axis=1)
+    # print("inArr=",inArr)
+    # print("outArr=",outArr)
     return(outArr)
 
 def rawTomg(hmpVal):
@@ -51,31 +52,20 @@ def readFile(fname, trim=0):
                             skiprows = 1  # Skip the first (header) row
         )
         #print("rawArr = ",rawArr)
-        # Split the class identifier column from the 3 acceleration columns
-        classArr = rawArr[:,3]
-        accArr = rawArr[:,[0,1,2]]
-        # Calculate the magnitude of the acceleration vectors
-        magArr = getMagnitude(accArr)
-        # And combine the acceleration magnitude and class id back into a 2d array.
-        magArr = np.stack((magArr,classArr),axis=-1)
-        # print("classArr=",classArr,classArr.shape)
-        # print("accArr=",accArr,accArr.shape)
-        # print("magArr = ",magArr, magArr.shape)
-
         
-
         # Now split the array into separate arrays where the class column (1)
         # Changes.
         # See https://stackoverflow.com/questions/31863083/python-split-numpy-array-based-on-values-in-the-array
         # Basically
-        #    arr[:,1] selects all the column 1 values.
+        #    arr[:,3] selects all the column 1 values.
         #    np.diff calculates the difference between consecutive values.
         #    np.where selects non-zero differences.....
         # >>> np.split(arr, np.where(np.diff(arr[:,1]))[0]+1)
 
-        splitArr = np.split(magArr,np.where(np.diff(magArr[:,1]))[0]+1)
+        splitArr = np.split(rawArr,np.where(np.diff(rawArr[:,3]))[0]+1)
         # This gives us splitArr, which is a list of np arrays
         # print("splitArr=",len(splitArr),splitArr[0],splitArr[0].shape,splitArr[1].shape)
+
 
     else:
         print("readFile: ERROR - %s does not exist" % fname)
@@ -85,18 +75,37 @@ def readFile(fname, trim=0):
     nTrim = int(trim * RAW_FREQ)
     procArrs = []
     for eventArr in splitArr:
-        if (eventArr.shape[0]<=(2*nTrim)):
+        # print("readFile: eventArr mean=%f, max=%f, min=%f"
+        #       % ( eventArr.mean(), eventArr.max(),eventArr.min()), eventArr.shape)
+        
+        reSampArr = reSample(eventArr,RAW_FREQ,PROC_FREQ)
+        print("Re-sampled from %d points to %d points" %
+              (eventArr.shape[0], reSampArr.shape[0]), reSampArr.shape)
+        # print("readFile: reSampArr mean=%f, max=%f, min=%f"
+        #       % ( reSampArr.mean(), reSampArr.max(),reSampArr.min()))
+        if (reSampArr.shape[0]<=(2*nTrim)):
             print("Dataset too short to trim - using full dataset")
         else:
             # print ("Trim=%.1f, nTrim=%d" % (trim,nTrim))
             # print(eventArr.shape)
-            eventArr = eventArr[nTrim:eventArr.shape[0]-nTrim]
+            reSampArr = reSampArr[nTrim:eventArr.shape[0]-nTrim]
             # print(eventArr.shape)
-        reSampArr = reSample(eventArr,RAW_FREQ,PROC_FREQ)
-        print("Re-sampled from %d points to %d points" %
-              (eventArr.shape[0], reSampArr.shape[0]))
-        procArrs.append(reSampArr)
-    # print("procArrs = ",len(procArrs),procArrs[0],procArrs[0].shape)
+
+        # Split the class identifier column from the 3 acceleration columns
+        classArr = reSampArr[:,3]
+        accArr = reSampArr[:,[0,1,2]]
+        # Calculate the magnitude of the acceleration vectors
+        magArr = getMagnitude(accArr)
+        # And combine the acceleration magnitude and class id back into a 2d array.
+        magArr = np.stack((magArr,classArr),axis=-1)
+        # print("classArr=",classArr,classArr.shape)
+        # print("accArr=",accArr,accArr.shape)
+        # print("magArr = ",magArr, magArr.shape)
+        print("readFile: magArr mean=%f, max=%f, min=%f"
+              % ( magArr.mean(), magArr.max(),magArr.min()))
+
+        procArrs.append(magArr)
+    print("procArrs = ",len(procArrs),procArrs[0],procArrs[0].shape)
     return procArrs
 
 
@@ -107,29 +116,31 @@ def reSample(inArr, inFreq = 32, outFreq=25):
     return outArr
 
 
-def writeData(catName, dataArr, outDir, dataStep = 3):
+def writeData(catName, dataArr, outDir = "SmartFall_output", nSamp = 125, dataStep = 3):
     """ Appends the data in dataArr to output CSV file <outDir>/<catName>.txt
     Each SAMP_TIME period of data is written on a separate line.
     The start position in dataArr is incremented by dataStep and a new line
     written until the end of dataArr is reached.
     """
+    # print("writeData - outDir=%s." % outDir)
+    print("dataArr mean=%f, max=%f, min=%f"
+          % ( dataArr.mean(), dataArr.max(),dataArr.min()))
     if (not os.path.exists(outDir)):
         os.makedirs(outDir)
     outFname = os.path.join(outDir, "%s.csv" % catName)
 
-    nSamp = int(SAMP_TIME * PROC_FREQ)
     f = open(outFname,"a")
 
     startPos = 0
 
-    while ((startPos + nSamp) < len(dataArr)):
-        print("Writing from startPos %d" % startPos)
+    while ((startPos + nSamp) <= len(dataArr)):
+        # print("Writing from startPos %d" % startPos)
         outStr = ""
         for n in range(0,nSamp):
             if (n>0):
-                outStr = "%s, %0.1f" % (outStr,dataArr[startPos+n])
+                outStr = "%s, %0.1f" % (outStr,dataArr[startPos+n,0])
             else:
-                outStr = "%s, %0.1f" % (catName,dataArr[startPos+n])
+                outStr = "%s, %0.1f" % (catName,dataArr[startPos+n,0])
         f.write(outStr)
         f.write("\n")
         startPos += dataStep
@@ -160,6 +171,10 @@ if (__name__ == "__main__"):
     print("Reading file %s" % fname)
     procArrs = readFile(fname, float(args['trim']))
 
+    #for eventArr in procArrs:
+    #    print("eventArr mean=%f, max=%f, min=%f"
+    #          % ( eventArr.mean(), eventArr.max(),eventArr.min()))
+    
     for eventArr in procArrs:
         if (eventArr[0][1]):
             print("Found Fall Event")
@@ -176,6 +191,9 @@ if (__name__ == "__main__"):
         if (args['plot']):
             plt.plot(xRaw,rawArr)
             plt.show()
-        #writeData(className, eventArr, args['outDir'])
+        #nSamp = int(SAMP_TIME * PROC_FREQ)
+        nSamp = 20   # Because this is the length of the re-sampled SmartFall
+                     # fall events.
+        writeData(className, eventArr, outDir = args['outDir'], nSamp = nSamp)
                 
             
